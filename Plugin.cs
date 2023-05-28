cfd054e4db7913fa21003f408615a70425a4cde3
Nodus Cursorius <NodusCursorius@users.noreply.github.com>
2023-05-28 14:52:55 -0700

    Initial commit
    I have no idea what I'm doing

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using ProjectM.UI;
+using UnityEngine;
+
+namespace RemoveVersionWatermark;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class RemoveVersionWatermarkPlugin : BasePlugin
+{
+	private static Harmony harmony;
+
+	public override void Load()
+	{
+		harmony = Harmony.CreateAndPatchAll(typeof(RemoveVersionWatermarkPlugin));
+		Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+	}
+
+	[HarmonyPostfix]
+	[HarmonyPatch(typeof(ConnectingView), nameof(ConnectingView.BackgroundButton_OnClick))]
+	private static void BackgroundButton_OnClick()
+	{
+		var versionStringObject = GameObject.Find("VersionString");
+		if (versionStringObject != null) {
+			versionStringObject.active = false;
+		}
+		
+	}
+}
